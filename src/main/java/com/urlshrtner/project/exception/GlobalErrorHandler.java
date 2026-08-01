1eeb0b327ddf4fc9cd2e216dedd029940f0feb5e
Kalkidan407 <kelemework267@gmail.com>
2026-08-01 01:30:29 -0400

    test:add test for createShortURL method

--- src/main/java/com/urlshrtner/project/exception/GlobalErrorHandler.java
+++ src/main/java/com/urlshrtner/project/exception/GlobalErrorHandler.java
@@ -11,27 +11,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-
-
-
-
 @RestControllerAdvice
 public class GlobalErrorHandler {
-
-     
 
     @ExceptionHandler(UrlNotFoundException.class)
     public ResponseEntity<ErrorResponse> urlNotFoundException(
          UrlNotFoundException exception,
-          HttpServletRequest request){
-    ErrorResponse errorResponse = ErrorResponse.builder()
+          HttpServletRequest request) {
+      ErrorResponse errorResponse = ErrorResponse.builder()
         .timestamp(LocalDateTime.now())
         .status(HttpStatus.NOT_FOUND.value())
         .message(exception.getMessage())
         .requestPath(request.getRequestURI())
         .build();
 
-     return ResponseEntity
+       return ResponseEntity
         .status(HttpStatus.NOT_FOUND)
         .body(errorResponse);
           
--- src/test/java/com/urlshrtner/project/services/  UrlServiceTest.java
+++ src/test/java/com/urlshrtner/project/services/  UrlServiceTest.java
@@ -15,6 +15,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.urlshrtner.project.dto.UrlRequest;
+import com.urlshrtner.project.dto.UrlResponse;
 import com.urlshrtner.project.exception.UrlNotFoundException;
 import com.urlshrtner.project.model.URLs;
 import com.urlshrtner.project.repositories.UrlRepository;
@@ -44,31 +46,88 @@ class UrlServiceTest {
         url.setClickCount(0L);
         url.setOriginalUrl("https://leetcode.com/u/Kalkidan-Kelemework/");
 
-        when(repository.findByShortCode("JHS7Vr"))
+        when(repository.findByShortCode("JHS7Vr") )
             .thenReturn(Optional.of(url));
 
-
-     String result = service.redirect("JHS7Vr");
+      String result = service.redirect("JHS7Vr");
     
-    assertEquals(1L, url.getClickCount());
-
+    assertEquals(1L, url.getClickCount());  //checks the state of the object
     assertEquals("https://leetcode.com/u/Kalkidan-Kelemework/", result);
-
-    verify(repository).save(url);
+    verify(repository).save(url); // this checks the behavior or interaction
 
     }
 
     @Test
 void shouldThrowUrlNotFoundExceptionWhenShortCodeDoesNotExist() {
+
     when(repository.findByShortCode("abc123"))
             .thenReturn(Optional.empty());
+
     assertThrows(
-            UrlNotFoundException.class,
+        UrlNotFoundException.class,
             () -> service.redirect("abc123")
     );
+
     verify(repository, never()).save(any());
 }
 
-  
+
+@Test
+void shouldIncreaseClickCountWhenRedirect(){
+
+    URLs url = new URLs();
+
+    url.setShortCode("abc123");
+    url.setOriginalUrl("https://google.com");
+    url.setClickCount(5L);
+
+
+    when(repository.findByShortCode("abc123"))
+            .thenReturn(Optional.of(url));
+
+    service.redirect("abc123");
+    assertEquals(6L, url.getClickCount());
+
+    verify(repository).save(url);
+}
+
+@Test
+void shouldCreateShortUrlSuccessfully() {
+
+       URLs url = new URLs();
+       UrlRequest request = new UrlRequest();
+       UrlResponse response = new UrlResponse();
+
+         request.setOriginalUrl("https://leetcode.com/u/Kalkidan-Kelemework/");
+         response.setShortCode("JHS7Vr");
+
+        url.setShortCode("JHS7Vr");
+        url.setOriginalUrl("https://leetcode.com/u/Kalkidan-Kelemework/");
+
+     when(generate.generate()).thenReturn("JHS7Vr");
+     when(mapper.toEntity(request)).thenReturn(url);
+     when(repository.save(url)).thenReturn(url);
+     when(mapper.toResponse(url)).thenReturn(response);
+
+    UrlResponse  result = service.createShortURL(request);
+
+     assertEquals("JHS7Vr", result.getShortCode());
+   
+
+     verify(generate).generate();
+     verify(repository).save(url);
+     verify(mapper).toEntity(request);
+     verify(mapper).toResponse(url);
+
+
 
 }
+
+
+
+
+
+
+}
+
+
